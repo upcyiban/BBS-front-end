e5005f1ef885e0ae8ec174d2dadd6dfdbffcf04a
zhang-dc <1134128909@qq.com>
2019-02-10 20:21:31 +0800

    创建网页结构

---
+++ src/components/Index/Body.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main-container">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index-Body',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main-container {
+	width: 100%;
+}
+</style>
---
+++ src/components/Messages.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main-container">
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
