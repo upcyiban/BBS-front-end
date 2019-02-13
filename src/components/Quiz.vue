a87a1d0dedb18b8da20f419923a0b4a63aeff3fc
lonely-root-number2 <44315255+lonely-root-number2@users.noreply.github.com>
2019-02-13 15:33:13 +0800

    Update Quiz.vue

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="main-container">
+    <textarea id="byDY" placeholder="
+    
+  提供你的疑惑、问题相关的详细信息，以便老师能更好的帮助到你">
+    
+    </textarea>
+    <br><br>
+    <input type="radio" name="DY" class="DY_radio">公开  (所有人可见)<br>
+    <input type="radio" name="DY" class="DY_radio">私密  (问题以及回复仅提问者与老师可见)
+    <br>
+    <input type="button" id="DY_button" class="DY_radio" value="确认提问">
   </div>
+
 </template>
 
 <script>
@@ -10,6 +21,12 @@ export default {
     return {
 
     }
+  },
+  created(){
+
+  },
+  methods:{
+
   }
 }
 </script>
@@ -19,5 +36,25 @@ export default {
 .main-container {
   width: 100%;
   height: 100%;
+}
+*{margin:0;padding:0}
+#byDY{
+  display:block;
+  width:90%;
+  height:80%;
+  margin-top:10%;
+  margin-left:5%; /*或者auto考虑到移动端不存在页面大小移动，建议使用固定百分比*/
+  border: solid 2px #63B8FF;
+  font-size: 1.5em;
+}
+.DY_radio{
+  margin-left:5%;
+}
+#DY_button{
+  display:block;
+  width:90%;
+  height:5%;
+  margin-top:5%;
+  font-size: 1em;
 }
 </style>
