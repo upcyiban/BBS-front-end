feef295f1b7705158ac77ec762bd0df95680a9cc
lonely-root-number2 <44315255+lonely-root-number2@users.noreply.github.com>
2019-02-13 20:29:55 +0800

    Update Quiz.vue

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -6,8 +6,8 @@
     
     </textarea>
    
-    <input type="radio" name="DY" class="DY_radio dy_flex" checked="checked">公开  (所有人可见)<br>
-    <input type="radio" name="DY" class="DY_radio dy_flex">私密  (问题以及回复仅提问者与老师可见)
+    <label><input type="radio" name="DY" class="DY_radio" checked="checked">公开  (所有人可见)</label>
+<br><label><input type="radio" name="DY" class="DY_radio">私密  (问题以及回复仅提问者与老师可见)</label>
     
     <input type="button" id="DY_button" class="DY_radio dy_flex" value="确认提问">
   </div>
@@ -48,6 +48,8 @@ export default {
   outline: none;
   border-radius:15px; 
   font-size: 1.5em;
+  padding: 2px;
+  box-sizing: border-box;
 }
 .DY_radio{
   margin-left:5%;
@@ -58,6 +60,8 @@ export default {
   height:5%;
   margin-top:5%;
   font-size: 1em;
+  color:#ffffff;
+  background-color:#3385FF;
 }
 /*.dy_flex{
   display: flex;
